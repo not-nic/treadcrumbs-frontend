0e83f98718519139b0056c4c40eb201e9d0370ba
notnic <nicovo989@gmail.com>
2023-07-05 00:53:59 +0100

    Created Dashboard.vue view.

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>TreadCrumbs</h1>
+    <vehicles></vehicles>
+  </div>
+</template>
+
+<script>
+import Vehicles from "../components/Vehicle/Vehicles.vue";
+export default {
+  name: "Dashboard",
+  components: {Vehicles}
+}
+</script>
+
+<style scoped>
+
+</style>
