6df01985932e85cf12bb83982cfc64b086eb72fa
notnic <nicovo989@gmail.com>
2023-07-05 17:36:42 +0100

    Add EditVehicle component

--- src/components/Vehicle/Vehicles.vue
+++ src/components/Vehicle/Vehicles.vue
@@ -1,28 +1,32 @@
 <template>
-  <div class="vehicles">
-    <div class="dashboard-element">
-      <h2>Vehicle Maintenance</h2>
-      <svg class="refresh" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10 20C7.22917 20 4.86979 19.026 2.92188 17.0781C0.973959 15.1302 0 12.7708 0 10C0 7.22917 0.973959 4.86979 2.92188 2.92188C4.86979 0.973959 7.22917 0 10 0C11.7708 0 13.3229 0.359375 14.6563 1.07813C15.1538 1.34633 15.6267 1.65153 16.0749 1.99371C16.8304 2.57042 18.125 2.08344 18.125 1.13302V0.9375C18.125 0.419733 18.5447 0 19.0625 0V0C19.5803 0 20 0.419733 20 0.9375V6.9375C20 7.48979 19.5523 7.9375 19 7.9375H13C12.4822 7.9375 12.0625 7.51777 12.0625 7V7C12.0625 6.48223 12.4822 6.0625 13 6.0625H15.4955C16.2836 6.0625 16.7435 5.20102 16.2075 4.62323C15.6398 4.01119 14.9977 3.48052 14.2813 3.03125C13.0521 2.26042 11.625 1.875 10 1.875C7.72917 1.875 5.80729 2.66146 4.23437 4.23437C2.66146 5.80729 1.875 7.72917 1.875 10C1.875 12.2708 2.66146 14.1927 4.23437 15.7656C5.80729 17.3385 7.72917 18.125 10 18.125C11.7292 18.125 13.3125 17.6302 14.75 16.6406C15.9956 15.7832 16.9127 14.6872 17.5013 13.3527C17.6658 12.9797 18.0234 12.7188 18.4311 12.7188V12.7188C19.063 12.7188 19.5224 13.3227 19.2836 13.9077C18.6117 15.5534 17.5484 16.9174 16.0938 18C14.3021 19.3333 12.2708 20 10 20Z" fill="#B2C5E1"/>
-      </svg>
-    </div>
 
+  <EditVehicle @close="isEditingVehicle=false" v-show="isEditingVehicle"></EditVehicle>
 
-    <div class="vehicle" v-for="vehicle in vehicles" :key="vehicle.id">
-      <vehicle-entry :id="vehicle.id"
-                     :name="vehicle.name"
-                     :position="new Coordinate(vehicle.position.x, vehicle.position.y, vehicle.position.z)"
-                     :last-job="vehicle.aiLastJob"
-                     :property-state="vehicle.propertyState"
-                     :age="vehicle.age"
-                     :price="vehicle.price"
-                     :operating-time="vehicle.operatingTime"
-                     :wear="vehicle.damage"
-                     :farm-id="vehicle.farmId"
-                     :number-plate="vehicle.licencePlate"
-                     :fuel="vehicle.fuel"
-                     :attachment="vehicle.attachment.attachmentIds"
-      ></vehicle-entry>
+  <div class="container">
+    <div class="vehicles">
+      <div class="dashboard-element">
+        <h2>Vehicle Maintenance</h2>
+        <svg @click="getData()" class="refresh" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M10 20C7.22917 20 4.86979 19.026 2.92188 17.0781C0.973959 15.1302 0 12.7708 0 10C0 7.22917 0.973959 4.86979 2.92188 2.92188C4.86979 0.973959 7.22917 0 10 0C11.7708 0 13.3229 0.359375 14.6563 1.07813C15.1538 1.34633 15.6267 1.65153 16.0749 1.99371C16.8304 2.57042 18.125 2.08344 18.125 1.13302V0.9375C18.125 0.419733 18.5447 0 19.0625 0V0C19.5803 0 20 0.419733 20 0.9375V6.9375C20 7.48979 19.5523 7.9375 19 7.9375H13C12.4822 7.9375 12.0625 7.51777 12.0625 7V7C12.0625 6.48223 12.4822 6.0625 13 6.0625H15.4955C16.2836 6.0625 16.7435 5.20102 16.2075 4.62323C15.6398 4.01119 14.9977 3.48052 14.2813 3.03125C13.0521 2.26042 11.625 1.875 10 1.875C7.72917 1.875 5.80729 2.66146 4.23437 4.23437C2.66146 5.80729 1.875 7.72917 1.875 10C1.875 12.2708 2.66146 14.1927 4.23437 15.7656C5.80729 17.3385 7.72917 18.125 10 18.125C11.7292 18.125 13.3125 17.6302 14.75 16.6406C15.9956 15.7832 16.9127 14.6872 17.5013 13.3527C17.6658 12.9797 18.0234 12.7188 18.4311 12.7188V12.7188C19.063 12.7188 19.5224 13.3227 19.2836 13.9077C18.6117 15.5534 17.5484 16.9174 16.0938 18C14.3021 19.3333 12.2708 20 10 20Z" fill="#B2C5E1"/>
+        </svg>
+      </div>
+      <div class="vehicle" v-for="vehicle in vehicles" :key="vehicle.id">
+        <vehicle-entry @Open="isEditingVehicle = true"
+                       :id="vehicle.id"
+                       :name="vehicle.name"
+                       :position="new Coordinate(vehicle.position.x, vehicle.position.y, vehicle.position.z)"
+                       :last-job="vehicle.aiLastJob"
+                       :property-state="vehicle.propertyState"
+                       :age="vehicle.age"
+                       :price="vehicle.price"
+                       :operating-time="vehicle.operatingTime"
+                       :wear="vehicle.damage"
+                       :farm-id="vehicle.farmId"
+                       :number-plate="vehicle.licencePlate"
+                       :fuel="vehicle.fuel"
+                       :attachment="vehicle.attachment.attachmentIds"
+        ></vehicle-entry>
+      </div>
     </div>
   </div>
 </template>
@@ -32,6 +36,7 @@ import { defineComponent } from "vue";
 import  axios from "axios";
 import VehicleEntry from "./VehicleEntry.vue";
 import { Coordinate } from "../../ts/Coordinate.ts";
+import EditVehicle from "./EditVehicle.vue";
 
 export default defineComponent({
   name: "Vehicles",
@@ -43,12 +48,14 @@ export default defineComponent({
   },
 
   components: {
+    EditVehicle,
     VehicleEntry
   },
 
   data() {
     return {
       vehicles: [] as any[],
+      isEditingVehicle: false,
     }
   },
 
@@ -82,6 +89,11 @@ h2 {
   text-align: start;
 }
 
+hr {
+  width: 25%;
+  color: #3F4753;
+}
+
 .dashboard-element {
   display: flex;
   flex-direction: row;
@@ -92,29 +104,40 @@ h2 {
 .refresh {
   max-width: 28px;
   padding: 5px;
+  transition: transform 0.6s cubic-bezier(0.83, 0, 0.17, 1);
 }
 
 .refresh:hover {
   max-width: 28px;
   padding: 5px;
   fill-opacity: 0.5;
+  transform: rotate(360deg);
 }
 
 .vehicle {
+  width: 100%;
   display: flex;
   flex-direction: column;
   padding: 5px;
-  gap: 20px;
+  gap: 5px;
+}
+
+.container {
+  width: 720px;
+  padding: 15px;
+  background-color: #212833;
+  border: 2px solid rgba(89, 89, 89, 0.30);
+  border-radius: 15px;
 }
 
 .vehicles {
   display: flex;
   flex-direction: column;
-  gap: 20px;
+  gap: 5px;
   background: rgba(0, 0, 0, 0.10);
   padding: 20px;
   max-height: 400px;
-  width: 700px;
+  width: 100%;
   overflow: auto;
   border: 2px solid rgba(89, 89, 89, 0.30);
   border-radius: 15px;
