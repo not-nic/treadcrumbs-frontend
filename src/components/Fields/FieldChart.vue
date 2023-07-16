399dec09314150e389774ec09fc0e3a5e80e2e92
notnic <nicovo989@gmail.com>
2023-07-16 09:03:00 +0100

    Added vue.chartjs pie chart

---
+++ src/components/Fields/FieldChart.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import {Pie} from "vue-chartjs";
+
+import {ArcElement, Chart as ChartJS, Legend, Tooltip} from 'chart.js'
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default defineComponent({
+  name: "FieldChart",
+  components: {Pie},
+
+  props: {
+    data: {
+      type: Object,
+      required: true
+    },
+    options: {
+      type: Object,
+      default: () => {}
+    }
+  },
+
+  watch: {
+    data: {
+      immediate: true,
+      handler(newValue, oldValue) {
+        console.log('Data Changed:', newValue, oldValue)
+      }
+    }
+  }
+})
+</script>
+
+<template>
+  <Pie :data="data" :options="options"></Pie>
+</template>
+
+<style scoped>
+
+</style>
