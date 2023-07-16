9cf5325d237a02482ffb80384ffea53ce7e29a2b
notnic <nicovo989@gmail.com>
2023-07-16 09:01:15 +0100

    Add all dashboard elements.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,23 +1,43 @@
 <template>
-  <h1>TreadCrumbs</h1>
-  <div class="elements" >
-    <vehicles></vehicles>
-    <career></career>
+  <div class="elements">
+    <div class="top">
+      <vehicles></vehicles>
+      <fields></fields>
+      <career></career>
+    </div>
+    <div class="bottom">
+      <Map></Map>
+      <Note></Note>
+    </div>
   </div>
 </template>
 
 <script>
 import Vehicles from "../components/Vehicle/Vehicles.vue";
 import Career from "../components/Career/Career.vue";
+import Fields from "../components/Fields/Fields.vue";
+import Map from "../components/Map/Map.vue";
+import Note from "../components/Notes/Note.vue"
 export default {
   name: "Dashboard",
-  components: {Career, Vehicles}
+  components: {Fields, Career, Vehicles, Map, Note}
 }
 </script>
 
 <style scoped>
 .elements {
   display: flex;
+  flex-direction: column;
   gap: 15px;
+}
+
+.top {
+  display: flex;
+  gap: 15px;
+}
+
+.bottom {
+  display: flex;
+  gap: 15px
 }
 </style>
