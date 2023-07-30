5cfaa6cbbbbed880a0a7528beb69ddb0db059a55
notnic <nicovo989@gmail.com>
2023-07-31 00:54:28 +0100

    Refactor Note.vue

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -1,37 +1,53 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
 
+export interface Note {
+  id: Number
+  author: String,
+  contents: String,
+  created: Date
+}
+
 export default defineComponent({
-  name: "Note"
+  name: "NoteComponent",
+
+  props: {
+    note: {} as Note
+  }
 })
 </script>
 
 <template>
-<div class="container">
   <div class="note">
-
+    <div class="content">
+      <p>{{note.contents}}</p>
+    </div>
+    <div class="info">
+      <a>Info</a>
+    </div>
   </div>
-</div>
+  <div v-for="(value, key) in note.additionalNoteData" :key="key" class="extra">
+    {{key}} | {{value}}
+  </div>
 </template>
 
 <style scoped>
-.container {
-  width: 660px;
-  padding: 15px;
-  background-color: #212833;
-  border: 2px solid rgba(89, 89, 89, 0.30);
-  border-radius: 15px;
+
+p {
+  margin: 0;
+  padding: 0;
 }
 
 .note {
   display: flex;
-  flex-direction: column;
-  gap: 5px;
-  background: rgba(0, 0, 0, 0.10);
-  padding: 20px;
-  height: 100%;
-  overflow: auto;
-  border: 2px solid rgba(89, 89, 89, 0.30);
-  border-radius: 15px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 10px 5px 10px 5px;
+}
+
+.content {
+
 }
 </style>
