870d82179cb0641070058b669d6c1055da9f54fe
notnic <nicovo989@gmail.com>
2023-08-02 02:20:46 +0100

    Additional fieldwork commands added.

--- src/components/Notes/GeneratedNoteCreator.vue
+++ src/components/Notes/GeneratedNoteCreator.vue
@@ -18,16 +18,65 @@ export default defineComponent({
 
 <template>
   <div class="note-creator" v-show="noteStore.showNoteCreator">
-    <h4>Generate Notes</h4>
-    <generated-note :title="'/seed'"
-                    :subtext="'use this command to remind you to plant your next field.'"
-                    :arguments="[
+    <h3>Generate Notes</h3>
+    <div class="generated-notes">
+      <generated-note :url="'/fieldwork'"
+                      :title="'/Seed'"
+                      :subtext="'use this command to remind you to seed your next field.'"
+                      :arguments="[
                         {field: 0, type: 'number'},
                         {crop: '', type: 'text'},
                         {vehicle: 0, type: 'number'}
                     ]"
-                    :command="`/seed @arg with @arg using @arg`"
-    ></generated-note>
+                      :command="`/Seed @arg with @arg using @arg`"
+      ></generated-note>
+      <generated-note :url="'/fieldwork'"
+                      :title="'/Plant'"
+                      :subtext="'use this command to remind you to plant your next field.'"
+                      :arguments="[
+                        {field: 0, type: 'number'},
+                        {crop: '', type: 'text'},
+                        {vehicle: 0, type: 'number'}
+                    ]"
+                      :command="`/Plant @arg with @arg using @arg`"
+      ></generated-note>
+      <generated-note :url="'/fieldwork'"
+                      :title="'/Cultivate'"
+                      :subtext="'use this command for cultivating fields.'"
+                      :arguments="[
+                        {field: 0, type: 'number'},
+                        {vehicle: 0, type: 'number'}
+                    ]"
+                      :command="`/Cultivate @arg using @arg`"
+      ></generated-note>
+      <generated-note :url="'/fieldwork'"
+                      :title="'/Plow'"
+                      :subtext="'use this command for plowing fields.'"
+                      :arguments="[
+                        {field: 0, type: 'number'},
+                        {vehicle: 0, type: 'number'}
+                    ]"
+                      :command="`/Plow @arg using @arg`"
+      ></generated-note>
+      <generated-note :url="'/fieldwork'"
+                      :title="'/Mulch'"
+                      :subtext="'use this command for mulching fields.'"
+                      :arguments="[
+                        {field: 0, type: 'number'},
+                        {vehicle: 0, type: 'number'}
+                    ]"
+                      :command="`/Mulch @arg using @arg`"
+      ></generated-note>
+      <generated-note :url="'/fieldwork'"
+                      :title="'/Rolling'"
+                      :subtext="'use this command for rolling fields.'"
+                      :arguments="[
+                        {field: 0, type: 'number'},
+                        {vehicle: 0, type: 'number'}
+                    ]"
+                      :command="`/Rolling @arg using @arg`"
+      ></generated-note>
+    </div>
   </div>
 </template>
 
@@ -40,13 +89,21 @@ export default defineComponent({
   z-index: 2;
   position: absolute;
   border-radius: 10px;
+  padding: 20px;
+  overflow: auto;
 }
 
-h4 {
+h3 {
   margin: 0;
   font-family: 'Poppins', sans-serif;
-  color: #95a5bd;
+  color: rgba(178, 197, 225, 1);
   text-align: start;
-  padding: 15px;
+  padding-bottom: 15px;
+}
+
+.generated-notes {
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
 }
 </style>
