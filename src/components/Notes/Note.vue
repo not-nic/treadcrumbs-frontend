78c6dc2e60ac836b9f93c51a478bf754355189d7
notnic <nicovo989@gmail.com>
2023-08-03 23:44:20 +0100

    Function to add colour and crop icon to crop in seed command.

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import {defineComponent} from 'vue'
 import {useNoteStore} from "../../stores/NoteStore.ts";
+import {crops} from "../../ts/Crops.ts";
 
 export interface Note {
   id: Number
@@ -24,16 +25,41 @@ export default defineComponent({
     }
   },
 
+  computed: {
+    modifiedNoteContents(): string {
+      return this.replaceCropColorInNote(this.note.contents)
+    }
+  },
+
+  methods: {
+    isEmpty(additionalNoteData: Object) {
+      return additionalNoteData && Object.keys(additionalNoteData).length > 0;
+    },
+
+    replaceCropColorInNote(contents: string): string {
+      const cropRegex = /\[([^\]]+)\]/g;
+      return contents.replace(cropRegex, (match, cropName) => {
+        const crop = crops.find((c) => c.name.toLowerCase() === cropName.toLowerCase());
+
+        if (crop) {
+          return `<span style="color: ${crop.colour}; display: flex; flex-direction: row; align-items: center">[<img src="src/assets/crops/${crop.filepath}" alt="crop icon" width="20px"/> ${cropName}]</span>`
+        } else {
+          return match
+        }
+      });
+    }
+  }
+
 })
 </script>
 
 <template>
   <div class="note">
     <div class="content">
-      <p>{{note.contents}}</p>
+      <p v-html="modifiedNoteContents"></p>
     </div>
     <div class="info">
-      <a @click="noteStore.displayNoteInfo(note.id)">Info</a>
+      <a @click="noteStore.displayNoteInfo(note.id)" v-if="isEmpty(note.additionalNoteData)">Info</a>
     </div>
   </div>
   <div v-show="noteStore.showNoteInfo && noteStore.currentNoteId === note.id" v-for="(value, key) in note.additionalNoteData" :key="key" class="extra">
@@ -47,6 +73,9 @@ export default defineComponent({
 p {
   margin: 0;
   padding: 0;
+  text-align: start;
+  display: flex;
+  gap: 5px;
 }
 
 .note {
@@ -57,10 +86,17 @@ p {
   padding: 10px 5px 10px 5px;
   font-family: 'Overpass', sans-serif;
   color: rgba(178, 197, 225, 0.9);
+  border-radius: 8px;
+  gap: 10px;
+}
+
+.note:hover {
+  background-color: rgba(178, 197, 225, 0.1);
 }
 
 a {
   font-weight: 300;
+  padding-right: 10px;
 }
 
 .content {
@@ -75,7 +111,18 @@ a {
   padding-left: 30px;
 }
 
+.key {
+  font-family: 'Overpass', sans-serif;
+  color: #76879b;
+  padding: 0;
+  font-size: 16px;
+}
+
 .value {
+  font-family: 'Fira Code', monospace;
+  font-weight: 300;
   color: #B36A2E;
+  padding: 5px;
+  font-size: 16px;
 }
 </style>
