555b31c6403dc68d183e7d81aa368bc271f03d3e
notnic <nicovo989@gmail.com>
2023-08-03 23:43:04 +0100

    Visual changes to CreateNote.vue

--- src/components/Notes/CreateNote.vue
+++ src/components/Notes/CreateNote.vue
@@ -34,15 +34,17 @@ export default defineComponent({
 <template>
   <div class="create">
     <div v-if="!noteStore.isGeneratingNote" class="normal-note">
-      <textarea @keyup="showNoteCreator" v-model="noteStore.command" placeholder="Type '/' to get started..."></textarea>
-      <input type="submit" value="submit"/>
+      <textarea @keyup="showNoteCreator" v-model="noteStore.command" minlength="5" placeholder="Type '/' to get started..."></textarea>
+      <input class="submit" @click="noteStore.postRegularNote()" type="submit" value="Create"/>
     </div>
-    <div v-if="noteStore.isGeneratingNote" class="generated-command">
-      <span @click="noteStore.hideGeneratingNote()" >{{noteStore.title}}</span>
-      <div class="arguments"  v-for="arg in noteStore.arguments" :key="arg">
-        <note-argument :argument="arg" :type="arg.type"></note-argument>
+    <div v-if="noteStore.isGeneratingNote" class="command-container">
+      <div  class="generated-command">
+        <span @click="noteStore.hideGeneratingNote()" >{{noteStore.title}}</span>
+        <div class="arguments"  v-for="arg in noteStore.arguments" :key="arg">
+          <note-argument :argument="arg" :type="arg.type"></note-argument>
+        </div>
       </div>
-      <input @click="noteStore.createGeneratedNoteText" type="submit" value="submit"/>
+      <input class="submit" @click="noteStore.createGeneratedNoteText" type="submit" value="Create"/>
     </div>
   </div>
 </template>
@@ -58,14 +60,31 @@ textarea, .generated-command {
   padding: 10px;
 
   font-family: 'Fira Code', monospace;
+  color: #95A5BD;
 }
 
 textarea:focus {
   outline: none;
 }
 
-.generated-command, .normal-note{
+.generated-command, .normal-note, .command-container{
   display: flex;
   gap: 10px;
 }
+
+.submit {
+  padding: 10px;
+  font-family: 'Fira Code', monospace;
+  background-color: #1b2029;
+  border-radius: 10px;
+  color: #5c6879;
+  border: none;
+  transition: all 0.5s;
+}
+
+.submit:hover {
+  border: 1px solid #95A5BD;
+  color: #95A5BD;
+}
+
 </style>
